5bec38ba349124fa0218f1cd6310977d50afeb45
webngoc04 <126089189+webngoc04@users.noreply.github.com>
2023-07-09 20:57:31 +0700

    Create Chuyển F sang C .cpp

---
+++ Chuyển F sang C .cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+    long double C,F;
+
+    cin>>F;
+
+    C = (F-32)*5/9;
+    cout<<fixed<<setprecision(1)<<C<<endl;
+    return 0;
+
+}
